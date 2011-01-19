3856e23d956765c840c05f9b9f7162d95a30359b
Daniel Sabo <DanielSabo@gmail.com>
2011-01-19 01:49:48 -0800

    Add Licenses to help menu Give the app an icon Customize tags and save user tags

--- osm2spatialiteAppDelegate.py
+++ osm2spatialiteAppDelegate.py
@@ -8,6 +8,7 @@
 
 from Foundation import *
 from AppKit import *
+import copy
 import os.path
 import osm2spatialite
 
@@ -24,8 +25,18 @@ class osm2spatialiteAppDelegate(NSObject):
     importWindowCurrentStageDesc    = objc.IBOutlet()
     importWindowCurrentStageTime    = objc.IBOutlet()
     importWindowCurrentStagePercent = objc.IBOutlet()
+    
+    requiredTagsArrayController = objc.IBOutlet()
+    userTagsArrayController = objc.IBOutlet()
+    editTagsWindow = objc.IBOutlet()
+    userTagsTable = objc.IBOutlet()
+    
+    requiredTags = objc.ivar()
+    userTags = objc.ivar()
 
     def applicationDidFinishLaunching_(self, sender):
+        defaults = NSUserDefaults.standardUserDefaults()
+        
         try:
             import fileformat_pb2,osmformat_pb2
             hasProtoBuf = True
@@ -40,8 +51,26 @@ class osm2spatialiteAppDelegate(NSObject):
             alert = NSAlert.alertWithMessageText_defaultButton_alternateButton_otherButton_informativeTextWithFormat_(title, None, None, None, msg)
             alert.runModal()
             NSApp.terminate_(None)
-        print shapely
-
+        
+        self.willChangeValueForKey_("requiredTags")
+        self.requiredTags = [{"tag":tag} for tag in osm2spatialite.defaultConfig["tags"]]
+        self.didChangeValueForKey_("requiredTags")
+        
+        self.willChangeValueForKey_("userTags")
+        userTags = defaults.arrayForKey_("userTags")
+        
+        if not userTags:
+            self.userTags = []
+        else:
+            self.userTags = [{"tag":tag} for tag in userTags]
+        
+        self.didChangeValueForKey_("userTags")
+        
+        #self.addObserver_forKeyPath_options_context_(self, "userTags", NSKeyValueObservingOptionNew, None)
+    
+    def observeValueForKeyPath_ofObject_change_context_(self, keyPath, object, change, context):
+        pass
+    
     @objc.IBAction
     def selectOSMFile_(self, sender):
         panel = NSOpenPanel.alloc().init()
@@ -77,12 +106,16 @@ class osm2spatialiteAppDelegate(NSObject):
     def importThread_(self, args):
         pool = NSAutoreleasePool.alloc().init()
         
+        config = copy.deepcopy(osm2spatialite.defaultConfig)
+        if "tags" in args:
+            config["tags"] = args["tags"][:]
+        
         self.performSelectorOnMainThread_withObject_waitUntilDone_("mainThreadUpdate:", "Hello from the thread", False)
         osm2spatialite.reportStatus = self.importReportStatus
         osm2spatialite.reportDetailedProgress = self.importReportDetails
         db = osm2spatialite.trydb(args["outfile"], True)
         if db:
-            osm2spatialite.parseFile(args["infile"], db, config=osm2spatialite.defaultConfig, verbose=True)
+            osm2spatialite.parseFile(args["infile"], db, config=config, verbose=True)
             self.performSelectorOnMainThread_withObject_waitUntilDone_("mainThreadImportDone:", args["outfile"], False)
         else:
             self.performSelectorOnMainThread_withObject_waitUntilDone_("mainThreadAbortMsg:", "Could not open DB", False)
@@ -133,9 +166,18 @@ class osm2spatialiteAppDelegate(NSObject):
         self.mainWindow.orderOut_(self)
         infile = self.inputFilenameField.stringValue()
         outfile = self.outputFilenameField.stringValue()
+        
+        tags = list()
+        
+        for tag in self.requiredTags:
+            tags.append(tag["tag"])
+        for tag in self.userTags:
+            tags.append(tag["tag"])
+        
         threadParams = {
             "infile":infile,
             "outfile":outfile,
+            "tags":tags
             }
         
         NSThread.detachNewThreadSelector_toTarget_withObject_(self.importThread_,self,threadParams)
@@ -144,3 +186,38 @@ class osm2spatialiteAppDelegate(NSObject):
         
         self.importWindow.makeKeyAndOrderFront_(self)
         #FIXME: Validate input filenames
+    
+    @objc.IBAction
+    def showTagsWindow_(self, sender):
+        self.mainWindow.orderOut_(self)
+        self.editTagsWindow.makeKeyAndOrderFront_(self)
+    
+    @objc.IBAction
+    def addUserTag_(self, sender):
+        self.userTagsArrayController.addObject_({"tag":""})
+        self.userTagsTable.reloadData()
+        newRowNum = self.userTagsArrayController.selectionIndex()
+        self.userTagsTable.editColumn_row_withEvent_select_(0, newRowNum, None, True)
+    
+    @objc.IBAction
+    def closeTagsWindow_(self, sender):
+        self.requiredTagsArrayController.commitEditing()
+        self.userTagsArrayController.commitEditing()
+        self.editTagsWindow.orderOut_(self)
+        self.mainWindow.makeKeyAndOrderFront_(self)
+        
+        
+        newTags = list()
+        required = [x["tag"] for x in self.requiredTags]
+        for value in self.userTags:
+            tag = value["tag"]
+            if tag not in required and tag != "":
+                newTags.append(value)
+                
+        
+        defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject_forKey_([tag["tag"] for tag in newTags], "userTags")
+        
+        self.willChangeValueForKey_("userTags")
+        self.userTags = newTags
+        self.didChangeValueForKey_("userTags")
